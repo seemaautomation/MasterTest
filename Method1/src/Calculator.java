c15cdc3240b13820829a89d19bbcc6595b0dbbea
hp <hp@LAPTOP-7DV82TSO>
2018-12-08 19:46:16 +0530

    hup

---
+++ Method1/src/Calculator.java
@@ -0,0 +1,22 @@
+
+public class Calculator {
+	final String brand= "casion";
+	int add(int x, int y) {
+		int sum= x+y;
+		//	System.out.println(sum);
+		return sum;
+
+	}
+	int subtract(int w, int z) {
+		int sub= w-z;
+		return sub;
+
+	}
+	double add_decimal(double x, double y) {
+		double sum =x+y;
+		return sum;
+	}
+
+
+}
+
---
+++ Method1/src/TestCase.java
@@ -0,0 +1,17 @@
+
+public class TestCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Calculator mod_2018= new Calculator();
+int i= 10;
+int j= 20;
+int sum=mod_2018.add(i, j);
+int sub= mod_2018.subtract(i, j);
+System.out.println(sum);
+System.out.println(sub);
+double g= mod_2018.add_decimal(34.77, 444.8);
+System.out.println(g);
+	}
+
+}
